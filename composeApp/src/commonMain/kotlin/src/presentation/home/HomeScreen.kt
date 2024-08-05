1af1f0fedd0682802db62bb6845cf0b7e42cebc8
Naimul Kabir <kabirnayeem.99@gmail.com>
2024-08-05 18:39:40 +0600

    feat: set up home screen

--- composeApp/src/commonMain/kotlin/src/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/src/presentation/home/HomeScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.screen.uniqueScreenKey
 import cafe.adriel.voyager.koin.koinNavigatorScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import core.UiEvent
+import kotlinx.coroutines.flow.SharedFlow
 
 class HomeScreen : Screen {
     @Composable
@@ -19,12 +22,27 @@ class HomeScreen : Screen {
         val uiState = viewModel.uiState.collectAsState()
         val snackbarHostState = remember { SnackbarHostState() }
 
-        LaunchedEffect(true) { viewModel.fetchData() }
+        LaunchedEffect(uniqueScreenKey) {
+            viewModel.fetchData()
+            viewModel.uiEvent.handleUserMessage(snackbarHostState)
+        }
 
         HomeView(
             uiState = uiState.value,
             snackbarHostState = snackbarHostState,
             onTabChanged = { tab -> viewModel.selectTab(tab) },
         )
+    }
+
+    private suspend fun SharedFlow<UiEvent>.handleUserMessage(snackbarHostState: SnackbarHostState?) {
+        snackbarHostState?.let { sbs ->
+            collect { uiEvent ->
+                when (uiEvent) {
+                    is UiEvent.UserMessage -> uiEvent.message
+                    is UiEvent.UserMessageEnum -> uiEvent.enum.toString()
+                    else -> ""
+                }.also { message -> sbs.showSnackbar(message) }
+            }
+        }
     }
 }
